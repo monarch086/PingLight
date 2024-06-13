f2c9ce83081c876455f377dee0a04a4de037d803
Oleksandr Barsuk <sbarsuk88@gmail.com>
2024-06-14 00:08:26 +0300

    Clean up (#19)
    * clean up detector firmware
    
    * update test lambda code
    
    * Correct PingLight.Test
    
    ---------
    
    Co-authored-by: Oleksandr Barsuk <oleksandr.barsuk@ihsmarkit.com>

--- src/Lambdas/PingLight.Test/Function.cs
+++ src/Lambdas/PingLight.Test/Function.cs
@@ -1,22 +1,36 @@
-using Amazon.Lambda.Core;
+﻿using Amazon.Lambda.Core;
+using PingLight.Core;
+using PingLight.Core.Persistence;
+using System.Text;
 using System.Text.Json.Nodes;
 
-// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
 namespace PingLight.Test;
 
 public class Function
 {
-    /// <summary>
-    /// A simple function to test new functionality
-    /// </summary>
-    public void FunctionHandler(JsonObject input, ILambdaContext context)
+    public async Task FunctionHandler(JsonObject input, ILambdaContext context)
     {
-        // string env = Environment.GetEnvironmentVariable("environment");
-        // context.Logger.LogInformation($"Env var environment = {env}.");
+        var changesRepo = new ChangesRepository(context.Logger);
+        var kyivZone = TimeZoneInfo.FindSystemTimeZoneById("Europe/Kiev");
 
-        // {"Environment": "prod"}
-        context.Logger.LogInformation(input.ToJsonString());
+        var from = DateTime.Today.AddDays(-1).FromKyivTime();
+        var till = DateTime.Today.FromKyivTime();
+
+        context.Logger.LogInformation($"from: {from.Kind} - {from.ToString("o")}");
+        context.Logger.LogInformation($"till: {till.Kind} - {till.ToString("o")}");
+        context.Logger.LogInformation($"kyivZone: {kyivZone.StandardName} - {kyivZone.BaseUtcOffset}");
+
+        var deviceId = "S4D-12";
+
+        var changes = await changesRepo.GetChanges(deviceId, from, till);
+        var logMessage = new StringBuilder($"Found {changes.Count} changes: \n\r");
+        foreach (var change in changes)
+        {
+            logMessage.AppendLine($"{change.ChangeDate.ToString("o")}: {change.IsLight}");
+        }
+
+        context.Logger.LogInformation(logMessage.ToString());
     }
 }
