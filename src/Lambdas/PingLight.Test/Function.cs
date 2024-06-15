4e4a37e2738333ed39c80163a4af781033bed2f2
Oleksandr Barsuk <sbarsuk88@gmail.com>
2024-06-15 17:38:33 +0300

    Add separate env configs

--- src/Lambdas/PingLight.Test/Function.cs
+++ src/Lambdas/PingLight.Test/Function.cs
@@ -1,7 +1,6 @@
-﻿using Amazon.Lambda.Core;
-using PingLight.Core;
-using PingLight.Core.Persistence;
-using System.Text;
+﻿using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using PingLight.Core.HttpResponses;
 using System.Text.Json.Nodes;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -10,27 +9,15 @@ namespace PingLight.Test;
 
 public class Function
 {
-    public async Task FunctionHandler(JsonObject input, ILambdaContext context)
+    public async Task<APIGatewayProxyResponse> FunctionHandler(JsonObject input, ILambdaContext context)
     {
-        var changesRepo = new ChangesRepository(context.Logger);
-        var kyivZone = TimeZoneInfo.FindSystemTimeZoneById("Europe/Kiev");
+        var stage = Environment.GetEnvironmentVariable("STAGE");
+        var customEnv = Environment.GetEnvironmentVariable("CUSTOM_ENV");
 
-        var from = DateTime.Today.AddDays(-1).FromKyivTime();
-        var till = DateTime.Today.FromKyivTime();
+        var message = $"Hello from {customEnv} environment (stage: {stage})";
 
-        context.Logger.LogInformation($"from: {from.Kind} - {from.ToString("o")}");
-        context.Logger.LogInformation($"till: {till.Kind} - {till.ToString("o")}");
-        context.Logger.LogInformation($"kyivZone: {kyivZone.StandardName} - {kyivZone.BaseUtcOffset}");
+        context.Logger.LogInformation(message);
 
-        var deviceId = "S4D-12";
-
-        var changes = await changesRepo.GetChanges(deviceId, from, till);
-        var logMessage = new StringBuilder($"Found {changes.Count} changes: \n\r");
-        foreach (var change in changes)
-        {
-            logMessage.AppendLine($"{change.ChangeDate.ToString("o")}: {change.IsLight}");
-        }
-
-        context.Logger.LogInformation(logMessage.ToString());
+        return new SuccessResponse(message);
     }
 }
