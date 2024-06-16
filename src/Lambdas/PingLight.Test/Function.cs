bbca9a1040305cd7d2c729bb4d1f9019b8d8f2ae
Oleksandr Barsuk <sbarsuk88@gmail.com>
2024-06-17 00:51:26 +0300

    Implement copying lambda

--- src/Lambdas/PingLight.Test/Function.cs
+++ src/Lambdas/PingLight.Test/Function.cs
@@ -1,4 +1,6 @@
-﻿using Amazon.Lambda.APIGatewayEvents;
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
 using PingLight.Core.HttpResponses;
 using System.Text.Json.Nodes;
@@ -9,15 +11,53 @@ namespace PingLight.Test;
 
 public class Function
 {
+    private static readonly IAmazonDynamoDB DynamoDbClient = new AmazonDynamoDBClient();
+    private static readonly string SourceTableName = "PingLight.Changes";
+    private static readonly string DestinationTableName = "PingLight.prod.Changes";
+
     public async Task<APIGatewayProxyResponse> FunctionHandler(JsonObject input, ILambdaContext context)
     {
         var stage = Environment.GetEnvironmentVariable("STAGE");
         var customEnv = Environment.GetEnvironmentVariable("CUSTOM_ENV");
 
-        var message = $"Hello from {customEnv} environment (stage: {stage})";
+        //var message = $"Hello from {customEnv} environment (stage: {stage})";
+        //context.Logger.LogInformation(message);
 
-        context.Logger.LogInformation(message);
+        try
+        {
+            await CopyTableAsync(context);
 
-        return new SuccessResponse(message);
+            return new SuccessResponse("Data copy completed successfully.");
+        }
+        catch (Exception ex)
+        {
+            return new FailResponse(ex.ToString());
+        }
+    }
+
+    public static async Task CopyTableAsync(ILambdaContext context)
+    {
+        Console.WriteLine($"Copying data from {SourceTableName} to {DestinationTableName}...");
+
+        var scanRequest = new ScanRequest
+        {
+            TableName = SourceTableName
+        };
+
+        var scanResponse = await DynamoDbClient.ScanAsync(scanRequest);
+        context.Logger.LogInformation($"Found {scanResponse.Count} records.");
+
+        foreach (var item in scanResponse.Items)
+        {
+            var putItemRequest = new PutItemRequest
+            {
+                TableName = DestinationTableName,
+                Item = item
+            };
+
+            await DynamoDbClient.PutItemAsync(putItemRequest);
+        }
+
+        context.Logger.LogInformation("Data copy completed successfully.");
     }
 }
