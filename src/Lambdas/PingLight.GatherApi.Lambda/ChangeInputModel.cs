a06130b5ff9cb837bee408395add0f879657f83b
Oleksandr Barsuk <sbarsuk88@gmail.com>
2024-07-28 17:12:08 +0300

    Add changes endpoint (#28)
    * Add changes endpoint
    
    * Update AddChangeAsync in PingLight.GatherApi.Lambda
    
    * Update repos
    
    * Correct pings repo

---
+++ src/Lambdas/PingLight.GatherApi.Lambda/ChangeInputModel.cs
@@ -0,0 +1,14 @@
+﻿using PingLight.Core.Serializing;
+using System.Text.Json.Serialization;
+
+namespace PingLight.GatherApi.Lambda;
+
+internal class ChangeInputModel
+{
+    [JsonPropertyName("id")]
+    public string Id { get; set; }
+
+    [JsonPropertyName("isLight")]
+    [JsonConverter(typeof(BooleanConverter))]
+    public bool IsLight { get; set; }
+}
---
+++ src/PingLight.Core/Serializing/BoolConverter.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace PingLight.Core.Serializing
+{
+    public class BooleanConverter : JsonConverter<bool>
+    {
+        public override bool Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            switch (reader.TokenType)
+            {
+                case JsonTokenType.True:
+                    return true;
+                case JsonTokenType.False:
+                    return false;
+                case JsonTokenType.String:
+                    return reader.GetString() switch
+                    {
+                        "true" => true,
+                        "false" => false,
+                        _ => throw new JsonException()
+                    };
+                default:
+                    throw new JsonException();
+            }
+        }
+
+        public override void Write(Utf8JsonWriter writer, bool value, JsonSerializerOptions options)
+        {
+            writer.WriteBooleanValue(value);
+        }
+    }
+}
