1cd343f7af39b8e545c7793815d8ad585cb6e1f6
Oleksandr Barsuk <sbarsuk88@gmail.com>
2026-09-22 10:11:58 +0000

    Remove obsolete Docker publishing workflow and Terraform configs

--- infrastructure/environments/dev/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "pinglight-terraform-state-bucket"
-    key    = "dev/terraform.tfstate"
-    region = "eu-central-1"
-  }
-}
--- infrastructure/modules/rds-pg/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "db_instance_endpoint" {
-  value = aws_db_instance.this.endpoint
-}
-
-output "db_instance_name" {
-  value = aws_db_instance.this.db_name
-}
